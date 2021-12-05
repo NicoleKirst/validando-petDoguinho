1ccc02003e858208b2583b60796f196caffd7da7
NicoleKirst <nicolekirst@outlook.com>
2021-12-05 13:59:02 -0300

    continuação validação

--- js/validacao.js
+++ js/validacao.js
@@ -4,10 +4,54 @@ export function valida(input) {
     if(validadores[tipoDeInput]) {
         validadores[tipoDeInput](input)
     }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+    }
+}
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo de nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é valido.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
 }
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input)
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = ''
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro]
+        }
+    })
+
+    return mensagem
 }
 
 function validaDataNascimento(input) {
@@ -15,7 +59,7 @@ function validaDataNascimento(input) {
     let mensagem = ''
 
     if(!maiorQue18(dataRecebida)) {
-        mensagem = 'Você deve ser maior que 18 anos para se cadastrar'
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
    }
 
     input.setCustomValidity(mensagem)
