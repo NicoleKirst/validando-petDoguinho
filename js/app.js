62a2d58322df0bdb3038ab1c34f5701d2317489c
NicoleKirst <nicolekirst@outlook.com>
2021-12-11 11:51:48 -0300

    finalizei

--- js/app.js
+++ js/app.js
@@ -3,6 +3,18 @@ import { valida } from './validacao.js'
 const inputs = document.querySelectorAll('input')
 
 inputs.forEach(input => {
+    if(input.dataset.tipo == 'preco') {
+        //mask para o preço
+        SimpleMaskMoney.setMask(input, {   
+            prefix: 'R$ ',  
+            fixed: true, 
+            fractionDigits: 2 , 
+            DecimalSeparator: ',' , 
+            ThousandsSeparator: '.', 
+            cursor : 'end' 
+        })
+    }
+
     input.addEventListener('blur', (evento) => {
         valida(evento.target)
     })
--- js/validacao.js
+++ js/validacao.js
@@ -54,6 +54,9 @@ const mensagensDeErro = {
     },
     estado: {
         valueMissing: 'O campo estado não pode estar vazio.'
+    },
+    preco: {
+        valueMissing: 'O campo de preço não pode estar vazio.'
     }
 }
 
